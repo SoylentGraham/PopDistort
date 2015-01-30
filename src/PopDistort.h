683664809b51b961fcb8c046f44fba067b7c49fe
Graham Reeves <graham@grahamreeves.com>
2015-01-30 16:00:16 +0000

    Initial commit (copy of popfeatures)

---
+++ src/PopDistort.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <ofxSoylent.h>
+#include <SoyApp.h>
+#include <TJob.h>
+#include <TChannel.h>
+
+
+
+
+class TPopFeatures : public TJobHandler, public TChannelManager
+{
+public:
+	TPopFeatures();
+	
+	virtual void	AddChannel(std::shared_ptr<TChannel> Channel) override;
+
+	void			OnExit(TJobAndChannel& JobAndChannel);
+	void			OnGetFeature(TJobAndChannel& JobAndChannel);
+	void			OnFindFeature(TJobAndChannel& JobAndChannel);
+	void			OnTrackFeatures(TJobAndChannel& JobAndChannel);
+	void			OnFindInterestingFeatures(TJobAndChannel& JobAndChannel);
+	void			OnNewFrame(TJobAndChannel& JobAndChannel);
+	
+	void			OnSendPing(TJobAndChannel& JobAndChannel);
+	void			OnRePing(TJobAndChannel& JobAndChannel);
+
+	void			OnDecode(TJobAndChannel& JobAndChannel);
+	
+public:
+	Soy::Platform::TConsoleApp	mConsoleApp;
+};
+
+
+

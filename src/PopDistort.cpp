64b74e83a00336961c5f9acd1e3989c3fe7b79db
Graham Reeves <graham@grahamreeves.com>
2015-02-03 11:17:49 +0000

    Added first simple-scan and lots of view/screen/latlon conversion funcs

--- src/PopDistort.cpp
+++ src/PopDistort.cpp
@@ -28,9 +28,296 @@ bool SoyData_Impl<std::string>::Encode(const SoyData_Impl<vec2f>& Data)
 	return true;
 }
 
+void MakeAngles(ArrayBridge<float>&& AngleDegs,float From,float To,float DegStep)
+{
+	if ( From > To )
+		DegStep = -fabsf( DegStep );
+	
+	for ( float d=From;	d<To;	d+=DegStep )
+	{
+		AngleDegs.PushBack( d );
+	}
+}
+
+#define PI	kPiF
+#define kPiF			(3.14159265358979323846264338327950288f)
+#define kDegToRad		(kPiF / 180.0f)
+#define kRadToDeg		(180.0f / kPiF)
+
+//	LatLonToView
+vec3f LatLonToView(vec2f latlon)
+{
+	//	http://en.wikipedia.org/wiki/N-vector#Converting_latitude.2Flongitude_to_n-vector
+	auto latitude = latlon.x;
+	auto longitude = latlon.y;
+	auto las = sin(latitude);
+	auto lac = cos(latitude);
+	auto los = sin(longitude);
+	auto loc = cos(longitude);
+	
+	vec3f View( los * lac, las, loc * lac );
+	//assert(fabsf(result.Length() - 1.0f) < 0.01f);
+
+	return View;
+}
+
+vec2f ViewToLatLon(vec3f View3)
+{
+	//	http://en.wikipedia.org/wiki/N-vector#Converting_n-vector_to_latitude.2Flongitude
+	auto x = View3.x;
+	auto y = View3.y;
+	auto z = View3.z;
+	
+	/*
+	 $lat = atan( $z / sqrt( ($x*$x) + ($y*$y) ) );
+	 //$lat = asin( $z );
+	 $lon = 0;
+	 if ( $x != 0 )
+	 $lon = atan( $y / $x );
+	 */
+	
+	auto lat = atan2( x, z );
+	
+	//	normalise y
+	auto xz = sqrt( (x*x) + (z*z) );
+	auto normy = y / sqrt( (y*y) + (xz*xz) );
+	auto lon = asin( normy );
+	//$lon = atan2( $y, $xz );
+	
+	//	stretch longitude...
+	lon *= 2.0;
+	
+	return vec2f( lat, lon );
+}
+
+//	-PI...PI, -PI...PI
+vec2f ScreenEquirectToLatLon(vec2f ScreenPos,float Width,float Height)
+{
+	auto xmul = 2.0;
+	auto xsub = 1.0;
+	auto ymul = 2.0;
+	auto ysub = 1.0;
+	
+	auto xfract = ScreenPos.x / Width;
+	xfract *= xmul;
+	
+	auto yfract = (Height - ScreenPos.y) / Height;
+	yfract *= ymul;
+	
+	float lat = ( xfract - xsub) * kPiF;
+	float lon = ( yfract - ysub) * kPiF;
+	return vec2f( lat, lon );
+}
+
+
+vec2f LatLonToScreen(vec2f LatLon,float Width,float Height)
+{
+	//	-pi...pi -> -1...1
+	auto& lat = LatLon.x;
+	auto& lon = LatLon.y;
+	lat /= kPiF;
+	lon /= kPiF;
+	
+	//	-1..1 -> 0..2
+	lat += 1.0;
+	lon += 1.0;
+	
+	//	0..2 -> 0..1
+	lat /= 2.0;
+	lon /= 2.0;
+	
+	lon = 1.0 - lon;
+	lat *= Width;
+	lon *= Height;
+	
+	return vec2f( lat, lon );
+}
+
+
+/*
+
+
+
+vec2f ScreenToLatLong(float x, float y, float Width, float Height)
+{
+	static float xmul = 2.f;
+	static float xsub = 1.0f;
+	float xfract = x / Width;
+	xfract *= xmul;
+	
+	static float ysub = 0.5f;
+	static float ymul = 1.f;
+	float yfract = (Height - y) / Height;
+	yfract *= ymul;
+	
+	float lon = ( xfract - xsub) * kPiF;
+	float lat = ( yfract - ysub) * kPiF;
+	static float lonmin = lon;
+	static float latmin = lat;
+	static float lonmax = lon;
+	static float latmax = lat;
+	lonmin = std::min( lon, lonmin );
+	lonmax = std::max( lon, lonmax );
+	latmin = std::min( lat, latmin );
+	latmax = std::max( lat, latmax );
+	return vec2f( lat, lon );
+}
+
+
+vec3f LatLongToView(vec2f latlon)
+{
+	float latitude = latlon.x;
+	float longitude = latlon.y;
+	float las = sinf(latitude);
+	float lac = cosf(latitude);
+	float los = sinf(longitude);
+	float loc = cosf(longitude);
+	
+	vec3f result( los * lac, las, loc * lac );
+	assert(fabsf(result.Length() - 1.0f) < 0.01f);
+	
+	return result;
+}
+
+vec2f LatLongToScreen(vec2f LatLon)
+{
+	//	gr: get latest from panopoly...
+	
+	// The largest coordinate component determines which face we’re looking at.
+	//	coords = pixel (camera) normal
+	vec3f coords = VectorFromCoordsRad(Coords);
+	float ax = fabsf(coords.x);
+	float ay = fabsf(coords.y);
+	float az = fabsf(coords.z);
+	vec2f faceOffset;
+	float x, y;	//	offset from center of face
+	
+	assert(ax != 0.0f || ay != 0.0f || az != 0.0f);
+	
+	if (ax > ay && ax > az)
+	{//	facing x
+		x = coords.z / ax;
+		y = -coords.y / ax;
+		if (0 < coords.x)
+		{
+			x = -x;
+			faceOffset = pxPos;
+		}
+		else
+		{
+			faceOffset = nxPos;
+		}
+	}
+	else if (ay > ax && ay > az)
+	{//	facing y
+		x = coords.x / ay;
+		y = coords.z / ay;
+		if (0 < coords.y)
+		{
+			faceOffset = pyPos;
+		}
+		else
+		{
+			y = -y;
+			faceOffset = nyPos;
+		}
+	}
+	else
+	{//	must be facing z
+		x = coords.x / az;
+		y = -coords.y / az;
+		if (0 < coords.z)
+		{
+			faceOffset = pzPos;
+		}
+		else
+		{
+			x = -x;
+			faceOffset = nzPos;
+		}
+	}
+	
+	faceOffset.x *= mFaceSize.x;
+	faceOffset.y *= mFaceSize.y;
+	x = (x * mHalfSize.x) + mHalfSize.x;
+	y = (y * mHalfSize.y) + mHalfSize.y;
+	assert( x >= 0 && x <= mFaceSize.x );
+	assert( y >= 0 && y <= mFaceSize.y );
+	
+	
+	auto result = FPMSampleLinear( Pixels, x + faceOffset.x, y + faceOffset.y, TWrapMode::Clamp, TWrapMode::Clamp );
+	return result;
+}
+
+vec2f ViewToScreen(const SoyPixels& Image,vec3f View)
+{
+
+}
+
+vec3f EularToView(float Pitch,float Yaw,float Roll)
+{
+	
+}
+ */
+
+template<typename TESTFUNC>
+bool FindFirstMatch(vec2f& MatchLatLon,ArrayBridge<float>&& Lats,ArrayBridge<float>&& Lons,TESTFUNC TestFunc)
+{
+	for ( int pi=0;	pi<Lats.GetSize();	pi++ )
+	{
+		for ( int yi=0;	yi<Lons.GetSize();	yi++ )
+		{
+			vec2f LatLon( Lats[pi], Lons[yi] );
+
+			if ( !TestFunc( LatLon ) )
+				continue;
+			
+			MatchLatLon = LatLon;
+			return true;
+		}
+	}
+	return false;
+}
+
 vec2f FindHolePos(const SoyPixels& Image,std::stringstream& Error)
 {
 	//	search 3D space until we find a black pixel
+	
+	Array<float> Lats;
+	Array<float> Lons;
+	static float LatMin = -PI;
+	static float LatMax = PI;
+	static float LonMin = -PI;
+	static float LonMax = PI;
+	static float LatStep = 0.01f;
+	static float LonStep = 0.01f;
+	MakeAngles( GetArrayBridge(Lats), LatMin, LatMax, LatStep );
+	MakeAngles( GetArrayBridge(Lons), LonMin, LonMax, LonStep );
+
+	auto IsBlack = [Image](vec2f LatLon)
+	{
+		auto Screen2 = LatLonToScreen( LatLon, Image.GetWidth(), Image.GetHeight() );
+		Screen2.x = std::min<float>( Image.GetWidth()-1, std::max<float>( 0, Screen2.x ) );
+		Screen2.y = std::min<float>( Image.GetHeight()-1, std::max<float>( 0, Screen2.y ) );
+		auto r = Image.GetPixel( Screen2.x, Screen2.y, 0 );
+		auto g = Image.GetPixel( Screen2.x, Screen2.y, 1 );
+		auto b = Image.GetPixel( Screen2.x, Screen2.y, 2 );
+		//static int Tolerance = 10+5+5;	//	gr: rugby "black spot" isn't black at all!
+		static int Tolerance = 2;	//	gr: rugby "black spot" isn't black at all!
+		static int Min = 256+256+256;
+		Min = std::min( Min, r+g+b);
+		return (r+g+b) <= Tolerance;
+	};
+	
+	//	find first black spot
+	vec2f FirstBlackLatLon;
+	if ( !FindFirstMatch( FirstBlackLatLon, GetArrayBridge(Lats), GetArrayBridge(Lons), IsBlack ) )
+	{
+		Error << "Failed to find black pixel";
+		return vec2f(0,0);
+	}
+	
+	return LatLonToScreen( FirstBlackLatLon, Image.GetWidth(), Image.GetHeight() );
 	
 	//	nudge up in 3d until we find top3
 	//	nudge down until we find bottom3
